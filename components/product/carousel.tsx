ebdafacaaa1b665df526a4eb801e819de563a372
Janet <jane824691@gmail.com>
2026-01-28 14:10:14 +0800

    optimize: hide default photos if no info

--- components/product/carousel.tsx
+++ components/product/carousel.tsx
@@ -29,19 +29,19 @@ export default function Carousel({
 }: CarouselProps) {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null)
 
-const DEFAULT_IMG = '../image/product/638348807730300000 (1).jfif'
+  const DEFAULT_IMG = '../image/product/638348807730300000 (1).jfif'
 
-// 商品圖可能存在專案路徑裡，也可能存在Firebase的連結
-const getImagePath = (image?: string | null) => {
-  if (!image) return DEFAULT_IMG
-  if (image.startsWith('http')) return image
-  return `../image/product/${image}`
-}
+  // 商品圖可能存在專案路徑裡，也可能存在Firebase的連結
+  const getImagePath = (image?: string | null) => {
+    if (!image) return DEFAULT_IMG
+    if (image.startsWith('http')) return image
+    return `../image/product/${image}`
+  }
 
-const imagePath01 = getImagePath(firstImage)
-const imagePath02 = getImagePath(mainImage)
-const imagePath03 = getImagePath(secondaryImage)
-const imagePath04 = getImagePath(additionalImage)
+  const imagePath01 = getImagePath(firstImage)
+  const imagePath02 = getImagePath(mainImage)
+  const imagePath03 = getImagePath(secondaryImage)
+  const imagePath04 = getImagePath(additionalImage)
 
   return (
     <>
@@ -64,18 +64,21 @@ const imagePath04 = getImagePath(additionalImage)
         <SwiperSlide>
           <img src={imagePath01} className="mx-auto img-fluid w-100" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath02} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath03} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath04} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath01} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
+        {mainImage && (
+          <SwiperSlide>
+            <img src={imagePath02} className="mx-auto img-fluid w-100" />
+          </SwiperSlide>
+        )}
+        {secondaryImage && (
+          <SwiperSlide>
+            <img src={imagePath03} className="mx-auto img-fluid w-100" />
+          </SwiperSlide>
+        )}
+        {additionalImage && (
+          <SwiperSlide>
+            <img src={imagePath04} className="mx-auto img-fluid w-100" />
+          </SwiperSlide>
+        )}
       </Swiper>
       <Swiper
         onSwiper={setThumbsSwiper}
@@ -89,18 +92,24 @@ const imagePath04 = getImagePath(additionalImage)
         <SwiperSlide>
           <img src={imagePath01} className="mx-auto img-fluid w-100" />
         </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath02} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath03} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath04} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imagePath01} className="mx-auto img-fluid w-100" />
-        </SwiperSlide>
+
+        {mainImage && (
+          <SwiperSlide>
+            <img src={imagePath02} className="mx-auto img-fluid w-100" />
+          </SwiperSlide>
+        )}
+
+        {secondaryImage && (
+          <SwiperSlide>
+            <img src={imagePath03} className="mx-auto img-fluid w-100" />
+          </SwiperSlide>
+        )}
+
+        {additionalImage && (
+          <SwiperSlide>
+            <img src={imagePath04} className="mx-auto img-fluid w-100" />
+          </SwiperSlide>
+        )}
       </Swiper>
     </>
   )
